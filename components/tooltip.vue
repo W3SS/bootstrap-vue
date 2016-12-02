1f7df2ac8f47f34500c151f46371b29a6b076371
Jarasłaŭ Viktorčyk <ugzuzg@gmail.com>
2016-12-02 18:59:46 +0300

    working tooltips

--- components/tooltip.vue
+++ components/tooltip.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <span class="tooltip-trigger" ref="trigger"><slot></slot></span>
-    <div class="['tooltip','fade','in',tooltipAlignment]" ref="tooltip">
+    <span class="tooltip-trigger" ref="trigger" @mouseenter="toggle(true)" @mouseleave="toggle(false)"><slot></slot></span>
+    <div :class="['tooltip','fade','in',tooltipAlignment]" ref="tooltip">
       <div class="tooltip-inner">
         <div class="tooltip-arrow"></div>
         <div class="tooltip-content">
@@ -16,8 +16,75 @@
 </template>
 
 <script>
-  export default {
-    replace: true,
-    computed: {},
-  }
+import Tether from 'tether'
+
+export default {
+  props: {
+    position: {
+      type: String,
+      default: 'top',
+    },
+    text: {
+      type: String,
+      default: '',
+    }
+  },
+  replace: true,
+  computed: {
+    tooltipAlignment() {
+      return !this.position || this.position === `default` ? `tooltip-top` : `tooltip-${this.position}`
+    }
+  },
+  mounted() {
+    this._trigger = this.$refs.trigger.children[0]
+    this._tooltip = this.$refs.tooltip
+    this._tooltip.style.display = 'none'
+  },
+  methods: {
+    toggle(show) {
+      if (show) {
+        let position = {
+          attachment: 'bottom center',
+          targetAttachment: 'top center'
+        };
+        if (this.position === 'bottom') {
+          position = {
+            attachment: 'top center',
+            targetAttachment: 'bottom center'
+          }
+        }
+        if (this.position === 'left') {
+          position = {
+            attachment: 'middle right',
+            targetAttachment: 'middle left'
+          }
+        }
+        if (this.position === 'right') {
+          position = {
+            attachment: 'middle left',
+            targetAttachment: 'middle right'
+          }
+        }
+        this._tooltip.style.display = 'block';
+        this._tether = new Tether({
+          element: this._tooltip,
+          target: this._trigger,
+          attachment: position.attachment,
+          targetAttachment: position.targetAttachment,
+        })
+      } else {
+        if (this._tether) {
+          this._tooltip.style.display = 'none'
+          this._tether.destroy()
+        }
+      }
+    },
+  },
+}
 </script>
+
+<style scoped>
+.tooltip {
+  z-index: 9999;
+}
+</style>
